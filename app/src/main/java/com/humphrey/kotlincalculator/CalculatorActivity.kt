ebe0eaa5644079075b686a2b7ef0f5f518730b2d
Humphrey Manango <humpoxmanango@gmail.com>
2026-04-15 14:28:32 +0800

    task a1: add calculator state variables

--- app/src/main/java/com/humphrey/kotlincalculator/CalculatorActivity.kt
+++ app/src/main/java/com/humphrey/kotlincalculator/CalculatorActivity.kt
@@ -7,6 +7,11 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class CalculatorActivity : AppCompatActivity() {
+
+    var currentInput = ""
+    var firstOperand = 0.0
+    var currentOperator = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
---
+++ app/src/main/java/com/humphrey/kotlincalculator/Scratch.kt
@@ -0,0 +1,4 @@
+package com.humphrey.kotlincalculator
+
+class Scratch {
+}
