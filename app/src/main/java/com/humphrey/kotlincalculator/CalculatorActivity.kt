043350e60652dc86e8869961804f51158cedf101
Humphrey Manango <humpoxmanango@gmail.com>
2026-04-16 20:53:53 +0800

    task d1: calculate function using when expression

--- app/src/main/java/com/humphrey/kotlincalculator/CalculatorActivity.kt
+++ app/src/main/java/com/humphrey/kotlincalculator/CalculatorActivity.kt
@@ -7,6 +7,19 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class CalculatorActivity : AppCompatActivity() {
+
+    private var currentOperator: String = ""
+
+    fun calculate(a: Double, b: Double): Double {
+        return when (currentOperator) {
+            "+" -> a + b
+            "-" -> a - b
+            "*" -> a * b
+            "/" -> if (b != 0.0) a / b else 0.0
+            else -> 0.0
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
